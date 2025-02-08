4e626b3053cb8e3dfca4e015261c99c95e4de2bd
David Puckree <davidpuckree132@gmail.com>
2025-02-08 19:40:48 +0000

    chore made a change

--- DebugExample.java
+++ DebugExample.java
@@ -2,7 +2,7 @@ public class DebugExample {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        int sum = add(a, b)+10*2;
+        int sum = add(a, b)+10*5;
         System.out.println("Sum: " + sum);
     }
 
